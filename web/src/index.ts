c252d54d6c141dc7d32221710d31cd0235c90983
Thomas B <thomas@bjorkroth.se>
2025-02-05 21:40:45 +0100

    add cors to api

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
 import { Missions } from "./missions";
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
